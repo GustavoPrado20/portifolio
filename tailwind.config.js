3f0b0ba2553c2190159e3e8b3b483d2f4d433592
Gustavo Prado <89040269+GustavoPrado20@users.noreply.github.com>
2023-07-25 18:56:26 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/index.html", "./public/**/*{html, js}", "./views/**/*{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
