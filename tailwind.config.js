9a48dc32fadf638ffd2061f2149d4fa440204d99
Gustavo Prado <89040269+GustavoPrado20@users.noreply.github.com>
2023-07-26 22:05:14 -0300

    V 0
    começando do zero

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/index.html", "./public/**/*{html, js}", "./views/**/*{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
